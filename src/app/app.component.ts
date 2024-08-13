cae87ddf5c3a09cefc0d47ccd632ab4acf723e43
nasti <nastia.pyslar@gmail.com>
2024-08-13 14:49:24 +0300

    Client skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'Dating App';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response) => {
+        this.users = response
+      },
+      error: (error) => {
+        console.log(error)
+      },
+      complete: () => {
+        console.log("request is complete")
+      }
+    })
+  }
 }
